322ffab36bc55d247385c5e7042f728eaec293d6
zhaoyukun <zhaoyukun@enn.cn>
2025-07-21 15:45:01 +0800

    openFeign logger

--- service/service-order/src/main/java/com/spring/order/config/OrderConfig.java
+++ service/service-order/src/main/java/com/spring/order/config/OrderConfig.java
@@ -1,16 +1,22 @@
 package com.spring.order.config;
 
+import feign.Logger;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class OrderConfig  {
+public class OrderConfig {
+
+    @Bean
+    Logger.Level level() {
+        return Logger.Level.FULL;
+    }
 
     @Bean
     @LoadBalanced  // 注解式-自动获取配置中心 负载均衡
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }

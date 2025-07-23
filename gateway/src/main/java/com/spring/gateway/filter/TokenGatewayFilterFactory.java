fa1a049622038d4ccb5cbae5acaabd7db4e7412e
zhaoyukun <zhaoyukun@enn.cn>
2025-07-24 07:31:58 +0800

    gateway-自定义Filter2

--- gateway/src/main/java/com/spring/gateway/filter/TokenGatewayFilterFactory.java
+++ gateway/src/main/java/com/spring/gateway/filter/TokenGatewayFilterFactory.java
@@ -2,33 +2,28 @@ package com.spring.gateway.filter;
 
 import java.util.UUID;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+/**
+ * 自定义gateway过滤器工厂
+ */
 @Component
 public class TokenGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
 
     @Override
     public GatewayFilter apply(NameValueConfig config) {
-        return new GatewayFilter() {
-            @Override
-            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-                return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                    System.out.println("请求路径：" + exchange.getRequest().getURI());
-                    String value = config.getValue();
-                    if (value.equals("uuid")) {
-                        value = UUID.randomUUID().toString();
-                    }
-                    if (value.equals("jwt")) {
-                        value = "jwt-token";
-                    }
-                    exchange.getResponse().getHeaders().add(config.getName(), value);
-                }));
-
+        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            System.out.println("请求路径：" + exchange.getRequest().getURI());
+            String value = config.getValue();
+            if (value.equals("uuid")) {
+                value = UUID.randomUUID().toString();
             }
-        };
+            if (value.equals("jwt")) {
+                value = "jwt-token";
+            }
+            exchange.getResponse().getHeaders().add(config.getName(), value);
+        }));
     }
 }

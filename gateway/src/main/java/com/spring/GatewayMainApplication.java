749fa8ade25c22a504b680ed5ffcc578f249ee20
zhaoyukun <zhaoyukun@enn.cn>
2025-07-23 19:20:04 +0800

    gateway-配置2

---
+++ gateway/src/main/java/com/spring/GatewayMainApplication.java
@@ -0,0 +1,18 @@
+package com.spring;
+
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMainApplication.class, args);
+    }
+
+
+}
